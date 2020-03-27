5e822a24284e4836b6cf11bd942d1c5d5d35da4d
kenneth eng <califorykng5i5@gmail.com>
2020-03-27 13:49:08 -0700

    changed app.py. the application now display log output of prediction result. fix run flask app port in run_docker.sh file.

--- app.py
+++ app.py
@@ -58,10 +58,15 @@ def predict():
     LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
+
+    # Logging the prediction 
+    LOG.info(f"INFO in APP out prediction prediction: \n{prediction}")
+    
     # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
 
