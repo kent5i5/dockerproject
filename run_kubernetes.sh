8a99170f94636b3fe03ccc67de900934da38b888
kenneth eng <califorykng5i5@gmail.com>
2020-03-27 17:55:45 -0700

    complete run_kubernetes script and upload_docker.sh. run_kubernetes create pod using public image which run data analysis app. upload_docker script upload the image to docker hub repo.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,22 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="yinkin/udacity"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+# To check the error when fail: kubectl describe pod project4 
+# To delete fail pod: kubectl delete pod proj4 
+kubectl run proj4\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=proj4
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward proj4 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="yinkin/udacity"
 
 # Step 2:  
 # Authenticate & tag
+docker login -p --username=yinkin --email=califorykng5i5@gmail.com
+docker tag 4044257fc521 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push yinkin/udacity
